d76dd2d40bca6873db28ab184f5ded369ed421bd
Albert Krewinkel <albert@zeitkraut.de>
2021-11-23 10:03:40 +0100

    [API change] Add `Figure` block constructor
    The new Figure block represents a figure with attributes, caption,
    and arbitrary block content.
    
    Co-authored-by: Aner Lucero <4rgento@gmail.com>
    Co-authored-by: Christian Despres <christian.j.j.despres@gmail.com>

--- src/Text/Pandoc/Walk.hs
+++ src/Text/Pandoc/Walk.hs
@@ -492,6 +492,10 @@ walkBlockM f (Table attr capt as hs bs fs)
        bs' <- walkM f bs
        fs' <- walkM f fs
        return $ Table attr capt' as hs' bs' fs'
+walkBlockM f (Figure attr capt blks)
+  = do capt' <- walkM f capt
+       blks' <- walkM f blks
+       return $ Figure attr capt' blks'
 
 -- | Perform a query on elements nested below a @'Block'@ element by
 -- querying all directly nested lists of @Inline@s or @Block@s.
@@ -515,6 +519,9 @@ queryBlock f (Table _ capt _ hs bs fs)
     query f hs <>
     query f bs <>
     query f fs
+queryBlock f (Figure _ capt blks)
+  = query f capt <>
+    query f blks
 queryBlock f (Div _ bs)               = query f bs
 queryBlock _ Null                     = mempty
 

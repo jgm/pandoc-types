1ae5864abbf709101c2db49980d2b6c42b43ab0c
rebecca skinner <rebecca@rebeccaskinner.net>
2023-01-22 14:54:48 -0600

    update default input to unstable

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = {self, nixpkgs, flake-utils}:

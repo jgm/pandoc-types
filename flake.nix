b5122b393cb0d7ec20f722108ffbc3f0900559f2
rebecca skinner <rebecca@rebeccaskinner.net>
2023-01-22 14:47:45 -0600

    add flake

--- flake.nix
+++ flake.nix
@@ -1,29 +1,19 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      # let
-      #   mkPkg = env:
-      #     nixpkgs.haskellPackages.developPackage {
-      #       name = "pandoc-types";
-      #       root = nixpkgs.lib.sourceFilesBySufficies ./.
-      #         [ ".cabal"
-      #           ".project"
-      #           ".hs"
-      #           "LICENSE"
-      #           "stack.yaml"
-      #         ];
-      #     };
-      # in {
-      let
-        builder = extraHaskellPackages:
-          import ./default.nix { inherit extraHaskellPackages; pkgs = nixpkgs; };
-      in {
-        packages.pkg = builder (env: []);
-        defaultPackage = self.packages.${system}.pkg;
-        devShell = builder (hsPkgs: with hsPkgs; [hlint ghcid]);
-      });
+   inputs = {
+     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+     flake-utils.url = "github:numtide/flake-utils";
+   };
+  outputs = {self, nixpkgs, flake-utils}:
+     flake-utils.lib.eachDefaultSystem (system:
+       let
+         builder = extraHaskellPackages:
+           import ./default.nix { inherit extraHaskellPackages;
+                                  pkgs = nixpkgs.legacyPackages.${system};
+                                };
+       in
+         {
+           packages.pandoc-types = builder (env: []);
+           defaultPackage = self.packages.${system}.pandoc-types;
+           devShell = builder (hsPkgs: with hsPkgs; [hlint ghcid]);
+         });
 }

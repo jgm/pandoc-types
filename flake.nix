9565fc068a321757317934f6dc6df0965d8c6aaa
rebecca skinner <rebecca@rebeccaskinner.net>
2023-01-22 14:48:42 -0600

    formatting

--- flake.nix
+++ flake.nix
@@ -1,19 +1,19 @@
 {
-   inputs = {
-     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
-     flake-utils.url = "github:numtide/flake-utils";
-   };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
   outputs = {self, nixpkgs, flake-utils}:
-     flake-utils.lib.eachDefaultSystem (system:
-       let
-         builder = extraHaskellPackages:
-           import ./default.nix { inherit extraHaskellPackages;
-                                  pkgs = nixpkgs.legacyPackages.${system};
-                                };
-       in
-         {
-           packages.pandoc-types = builder (env: []);
-           defaultPackage = self.packages.${system}.pandoc-types;
-           devShell = builder (hsPkgs: with hsPkgs; [hlint ghcid]);
-         });
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        builder = extraHaskellPackages:
+          import ./default.nix { inherit extraHaskellPackages;
+                                 pkgs = nixpkgs.legacyPackages.${system};
+                               };
+      in
+        {
+          packages.pandoc-types = builder (env: []);
+          defaultPackage = self.packages.${system}.pandoc-types;
+          devShell = builder (hsPkgs: with hsPkgs; [hlint ghcid]);
+        });
 }
